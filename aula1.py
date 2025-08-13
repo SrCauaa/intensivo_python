d07f8d308196bdc56bca435bb832b6cf3e9cbd37
Cauã Santos <128437755+SrCauaa@users.noreply.github.com>
2025-08-13 11:25:21 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,7 @@
+aluno = input ("Digite seu nome ")
+idade = int(input("Digite sua idade "))
+
+print(f"Nome do aluno: {aluno}")
+print("tipo de variavel", type(idade))
+print("")
+print(f"Idade do aluno {idade}")
---
+++ teste.py
@@ -0,0 +1,18 @@
+# Anotações aula 
+# string = texto que o computador entende como dado geralmente usa aspas dupla "" ou única '';
+# input é um comando para o usuario digitar algo dentro dele
+# Float = número que pode ter vírgula (no computador, é com ponto .)
+# Int 
+
+#exercicios pegados no chatgpt
+int 
+if(nota>=7):
+    print("aprovado")
+
+    else if(nota=>5,7<):
+
+    print("recuperacao")
+
+    else(nota<5):
+
+    print("reprovado")
